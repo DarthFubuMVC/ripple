442ffced369fd9d915377260ce8e05d82ab7dfde
Alistair Bush <alistair.bush@gmail.com>
2013-09-30 23:45:54 +1300

    Fix rake create_gem which fails by copying files to file not a directory as expected.

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,4 @@
+# vim: tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab
 require 'fuburake'
 
 @solution = FubuRake::Solution.new do |sln|
@@ -33,9 +34,12 @@ end
 
 desc "Creates the gem for fubu.exe"
 task :create_gem => [:compile] do
-    require "rubygems/package"
+	require "rubygems/package"
+	include FileUtils
 	cleanDirectory 'bin';	
 	cleanDirectory 'pkg'
+	mkdir 'bin'
+	mkdir 'pkg'
 	
 	copyOutputFiles "src/ripple/bin/#{@solution.compilemode}", '*.dll', 'bin'
 	copyOutputFiles "src/ripple/bin/#{@solution.compilemode}", 'ripple.exe', 'bin'
@@ -71,5 +75,4 @@ task :create_gem => [:compile] do
 	FileUtils.mv "ripple-cli-#{@solution.options[:build_number]}.gem", "pkg/ripple-cli-#{@solution.options[:build_number]}.gem"
 	
 end
-
 
